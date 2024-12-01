1bbbb04bec334c4e34defb3e1d748bb5c965a982
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-12-01 16:00:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, redirect, url_for
 import sqlite3
 import os
 import pytesseract
-from pytesseract import TesseractNotFoundError
 from PIL import Image
 
 app = Flask(__name__)
